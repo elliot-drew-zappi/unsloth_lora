cc2401240943594c0582c5b34990eed2bfc36ddd
Elliot Drew <elliot-drew@zappistore.com>
2024-04-22 16:00:07 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,5 @@ setup(
     name='unslothlora',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        "torch",
-        "unsloth @ git+https://github.com/unslothai/unsloth.git",
-        "xformers",
-        "trl",
-        "peft",
-        "accelerate",
-        "bitsandbytes"
-    ],
+    install_requires=[], # xformers is a huge pita so we're not going to install it here. Colab script is the best way to install it.
 )
